bd1013c49b8001bd06a82fcdb68a7a283787c40f
KeyvanOh <156261972+KeyvanOh@users.noreply.github.com>
2024-02-06 19:16:55 +0900

    Update Main.java

--- test6/Main.java
+++ test6/Main.java
@@ -1,10 +1,13 @@
+//
+//code2
+//
+//javac --module-path javafx-sdk-21.0.1\lib --add-modules javafx.controls,javafx.fxml test6\Main.java
 package test6;
-
 import javafx.application.Application;
-
 public class Main {
     public static void main(String[] args) {
         ChatClient.client();
         GUI.launch(GUI.class, args);
+        System.exit(0);
     }
 }
