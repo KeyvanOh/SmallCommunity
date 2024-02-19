a37f7b9809170e06cfa41d955f2814c94cc3e224
KeyvanOh <156261972+KeyvanOh@users.noreply.github.com>
2024-02-19 21:24:47 +0900

    Add files via upload

--- chat1/app/src/main/java/test2/GUI.java
+++ chat1/app/src/main/java/test2/GUI.java
@@ -16,6 +16,16 @@ import javafx.scene.shape.*;
 import javafx.scene.text.*;
 import javafx.scene.image.*;
 
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonToken;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class GUI extends Application {
     @Override
     public void start(Stage stage) throws FileNotFoundException, UnknownHostException {
@@ -99,6 +109,8 @@ public class GUI extends Application {
                 
             }
         };
+        buttonShow.addEventHandler(MouseEvent.MOUSE_ENTERED, eventHandlerText1);
+        
         EventHandler<MouseEvent> eventHandlerText2 = new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent e) {
@@ -106,11 +118,68 @@ public class GUI extends Application {
                 //System.out.println("MOUSE_EXITED");
             }
         };
-        buttonShow.addEventHandler(MouseEvent.MOUSE_ENTERED, eventHandlerText1);
         buttonShow.addEventHandler(MouseEvent.MOUSE_EXITED, eventHandlerText2);
         //buttonShow.addEventHandler(MouseEvent.MOUSE_MOVED, eventHandlerText2);
         
-        
+        EventHandler<MouseEvent> eventHandlerText3 = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                //buttonShow.setFill(Color.rgb(255, 0, 0));
+                //System.out.println();
+                
+                
+                try {
+                    JsonFactory jsonFactory = new JsonFactory();
+                    
+                    //JsonGenerator jsonGenerator =
+                    
+                    
+                    
+                    
+                    
+                    JsonParser jsonParser = jsonFactory.createParser(new File("port.json"));
+                    while (jsonParser.nextToken() != JsonToken.END_OBJECT) {
+                        String fieldName = jsonParser.getCurrentName();
+                        if ("port".equals(fieldName)) {
+                            jsonParser.nextToken();
+                            System.out.println(jsonParser.getText());
+                            
+                            
+                            Print.append("IPv4: ");
+                            Print.append(ipv4);
+                            Print.append("\nPort Number: ");
+                            //Print.append(portRand);
+                            //int port = 0;
+                            //port = jsonParser.getText();
+                            //port = Integer.parseInt(jsonParser.getText());
+                            int port = Integer.parseInt(jsonParser.getText());
+                            //Print.append(Mut.port);
+                            Print.append(port);
+                            
+                            text.setText(Print.getAndReset());
+                            
+                        };
+                    };
+                //} catch(Exception e) {
+                } catch(IOException err) {
+                    err.printStackTrace();
+                };
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+            }
+        };
+        buttonShow.addEventHandler(MouseEvent.MOUSE_CLICKED, eventHandlerText3);
         
         
         
